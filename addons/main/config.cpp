cf074c16fb5e470ee8e88b240ae521f6418c320d
Saveliy Tronza <sparker_95@mail.ru>
2019-02-22 15:45:39 +0300

    Update config.cpp

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -1,13 +1,13 @@
 class CfgPatches {
-	class intercept_template_plugin { //Change this
+	class arma_ofstream { //Change this
 		name = "Arma ofstream"; //Change this
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 1.82;
 		requiredAddons[] = {"intercept_core"};
 		author = "Sparker"; //Change this
-		authors[] = {"Dedmen"}; //Change this
-		url = "https://github.com/intercept/intercept-plugin-template"; //Change this
+		authors[] = {"Sparker"}; //Change this
+		url = "https://github.com/Sparker95/ARMA-ofstream"; //Change this
 		version = "1.0";
 		versionStr = "1.0";
 		versionAr[] = {1,0};
